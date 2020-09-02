37d34f360be0d8e1a9bf22888263a11a7d2674b5
Yoav Abadi <yoav.abadi@fiverr.com>
2020-09-02 08:26:57 +0300

    [FEAT] Add Pytest to workflow

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A Railway pattern based operation package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yoavabadi/WorkerOperation",
+    url="https://github.com/yoavabadi/Operation",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
