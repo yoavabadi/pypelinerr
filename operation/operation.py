7b75c99314b9ee986508bb406563a1fa5f225339
eyal segal <eyal.segal@aidoc.com>
2020-09-03 08:32:55 +0300

    move to other function

--- operation/operation.py
+++ operation/operation.py
@@ -3,9 +3,12 @@ from time import time
 from traceback import format_exc
 from typing import List
 
+from schema import Schema
+
 
 class Operation(ABC):
-    def __init__(self, options=None, entry_phase=None, schema: Schema):
+    def __init__(self, options=None, entry_phase=None, schema: Schema = None):
+        self.schema = schema
         self.options = options if options else {}
         self.operation_time = time()
         self._entry_phase = entry_phase
@@ -18,15 +21,7 @@ class Operation(ABC):
 
     def run(self):
         try:
-            phases = self.phases()
-            if self._entry_phase:
-                start_phase = phases.index(self._entry_phase)
-                phases = phases[start_phase:]
-
-            for phase in phases:
-                self.current_phase = phase
-                getattr(self, phase)()
-            self.success = True
+            self._run_phases()
         except Exception as e:
             if self.success:
                 self.break_phase = self.current_phase
@@ -49,3 +44,13 @@ class Operation(ABC):
     def fail_operation(self, message=None):
         self.success = False
         raise Exception(message)
+
+    def _run_phases(self):
+        phases = self.phases()
+        if self._entry_phase:
+            start_phase = phases.index(self._entry_phase)
+            phases = phases[start_phase:]
+        for phase in phases:
+            self.current_phase = phase
+            getattr(self, phase)()
+        self.success = True
