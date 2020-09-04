188b7e56c4521075ae1915d33faa016936ef84ef
Yoav Abadi <yoavabadi@gmail.com>
2020-09-04 22:32:38 +0300

    [INFRA] publish package on merge (#9)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/yoavabadi/operation",
-    requires=["schema>=0.7.2"],
+    requires=["schema"],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
