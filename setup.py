0461d9ba53986274789581696b85d2f6832913f5
Yoav Abadi <yoav.abadi@fiverr.com>
2020-09-11 16:23:44 +0300

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="operation",
-    version="0.0.2",
+    version="0.0.3",
     author="Yoav Abadi",
     author_email="yoavabadi@gmail.com",
     description="A Railway pattern based operation package",
