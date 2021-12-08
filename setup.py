fe60a9447ddc966a292738f41a4d6f3051f093e1
Yoav Abadi <yoavabadi@gmail.com>
2021-12-08 22:51:09 +0200

    bump-version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pypelinerr",
-    version="0.0.5",
+    version="0.0.6",
     author="Yoav Abadi",
     author_email="yoavabadi@gmail.com",
     description="A Railway pattern based pypeline package",
